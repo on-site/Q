4b7811fa0629f877d4ae1caf4608ff1aac09d3e3
Mike Stone <stonemj@gmail.com>
2013-01-21 00:33:48 -0800

    Stub tests for upcoming new functionality.

---
+++ test/src/com/on_site/q/QManipulationTest.java
@@ -0,0 +1,18 @@
+package com.on_site.q;
+
+import static com.on_site.q.Q.$;
+
+import com.google.common.base.Function;
+import com.on_site.util.TestBase;
+
+import org.testng.annotations.Test;
+
+public class QManipulationTest extends TestBase {
+    @Test
+    public void xml() throws Exception {
+    }
+
+    @Test
+    public void xmlXml() throws Exception {
+    }
+}

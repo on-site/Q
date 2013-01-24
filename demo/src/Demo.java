0f8c4f71243c3328bcb46f8addd4a14d5b3decee
Mike Stone <stonemj@gmail.com>
2013-01-23 23:47:00 -0800

    Make it possible to use $ in the demo.

--- demo/src/Demo.java
+++ demo/src/Demo.java
@@ -4,14 +4,8 @@ import java.io.InputStreamReader;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
-        if (args.length == 0 || "bsh".equals(args[0])) {
-            bsh();
-        }
-    }
-
-    public static void bsh() throws Exception {
         Interpreter interpreter = new Interpreter(new InputStreamReader(System.in), System.out, System.err, true);
-        interpreter.source("demo/demo.bsh");
+        interpreter.eval(new InputStreamReader(Demo.class.getResourceAsStream("demo.bsh")));
         interpreter.run();
     }
 }

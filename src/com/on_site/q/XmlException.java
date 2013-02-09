4ac32f2ad7edb459148b964556b610815924d124
Mike Virata-Stone <stonemj@gmail.com>
2013-02-08 18:59:35 -0800

    Add some comments that were missing in a couple classes.

--- src/com/on_site/q/XmlException.java
+++ src/com/on_site/q/XmlException.java
@@ -1,14 +1,36 @@
 package com.on_site.q;
 
+/**
+ * An exception that represents something gone wrong with parsing or
+ * otherwise processing XML.
+ *
+ * @author Mike Virata-Stone
+ */
 public class XmlException extends RuntimeException {
+    /**
+     * Construct the exception with the given message.
+     *
+     * @param message The message for this exception.
+     */
     public XmlException(String message) {
         super(message);
     }
 
+    /**
+     * Construct the exception with the given cause.
+     *
+     * @param cause The cause of this exception.
+     */
     public XmlException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Construct the exception with the given message and cause.
+     *
+     * @param message The message for this exception.
+     * @param cause The cause of this exception.
+     */
     public XmlException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/com/on_site/util/IOUtil.java
+++ src/com/on_site/util/IOUtil.java
@@ -8,11 +8,24 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.Reader;
 
+/**
+ * Utility class to allow input stream/reader utility functions.
+ *
+ * @author Mike Virata-Stone
+ */
 public class IOUtil {
     private IOUtil() {
         /* Disable instantiation for static class. */
     }
 
+    /**
+     * Join all the given streams into 1 continuous stream.  The
+     * streams are streamed in the order they are passed to the
+     * method.
+     *
+     * @param streams The streams to join.
+     * @return The joined streams into 1 InputStream.
+     */
     public static InputStream join(InputStream... streams) throws IOException {
         InputStreamSupplier[] suppliers = new InputStreamSupplier[streams.length];
 
@@ -23,6 +36,14 @@ public class IOUtil {
         return ByteStreams.join(suppliers).getInput();
     }
 
+    /**
+     * Join all the given readers into 1 continuous reader.  The
+     * readers are streamed in the order they are passed to the
+     * method.
+     *
+     * @param readers The readers to join.
+     * @return The joined readers into 1 Reader.
+     */
     public static Reader join(Reader... readers) throws IOException {
         ReaderSupplier[] suppliers = new ReaderSupplier[readers.length];
 
@@ -33,25 +54,51 @@ public class IOUtil {
         return CharStreams.join(suppliers).getInput();
     }
 
+    /**
+     * InputSupplier for InputStream objects.
+     */
     public static class InputStreamSupplier implements InputSupplier<InputStream> {
         private final InputStream stream;
 
+        /**
+         * Construct the supplier with the given stream.
+         *
+         * @param stream The stream to construct with.
+         */
         public InputStreamSupplier(InputStream stream) {
             this.stream = stream;
         }
 
+        /**
+         * Get the stream input.
+         *
+         * @return The stream this object was constructed with.
+         */
         public InputStream getInput() {
             return stream;
         }
     }
 
+    /**
+     * InputSupplier for Reader objects.
+     */
     public static class ReaderSupplier implements InputSupplier<Reader> {
         private final Reader reader;
 
+        /**
+         * Construct the supplier with the given reader.
+         *
+         * @param reader The reader to construct with.
+         */
         public ReaderSupplier(Reader reader) {
             this.reader = reader;
         }
 
+        /**
+         * Get the reader input.
+         *
+         * @return The reader this object was constructed with.
+         */
         public Reader getInput() {
             return reader;
         }
